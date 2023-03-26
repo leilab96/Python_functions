6b06facb7b37967a79a102ea6ff5cf93ea33a76c
Leila <badeeb9611@gmail.com>
2023-03-26 17:24:43 +0200

    correction

--- first.py
+++ first.py
@@ -1,12 +1,12 @@
 #First excercise
 #A function that finds all numbers between two arbitrary values (e.g. between 2000 and 3200) that can be divided by 7 but not by 5.
 
-def divisible_numbers(start, end):
+def divisible_numbers(start, end, divided, non_divided):
     numbers = []
     for num in range(start, end+1):
-        if num % 7 == 0 and num % 5 != 0:
+        if num % divided == 0 and num % non_divided != 0:
             numbers.append(num)
     return numbers
 
-print(divisible_numbers(0,35))
-print(divisible_numbers(2000,3200))
+print(divisible_numbers(0,35,7,5))
+print(divisible_numbers(2000,3200,7,5))
